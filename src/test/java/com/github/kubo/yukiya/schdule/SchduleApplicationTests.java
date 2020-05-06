9263ada6a244f13bd53fd058a4869a9f6ee0a7d5
kubo-yukiya <10003260s@gmail.com>
2020-05-06 17:05:12 +0900

    first commit

---
+++ src/test/java/com/github/kubo/yukiya/schdule/SchduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kubo.yukiya.schdule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
